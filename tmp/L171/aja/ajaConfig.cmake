ab09236b97f7b78d6f9a02255291d339cfdade0a
Jonathan Batchelor <jbatchelor@wetafx.co.nz>
2025-09-17 10:03:26 +1200

    HABITAT-746 Added missing temp aja/ffmpeg cmake modules.
    Signed-off-by: Jonathan Batchelor <jbatchelor@wetafx.co.nz>

---
+++ tmp/L171/aja/ajaConfig.cmake
@@ -0,0 +1,41 @@
+# Approved HACK, absolute nightmare, some black magic thing happening
+
+####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
+####### Any changes to this file will be overwritten by the next CMake run ####
+####### The input file was hello_worldConfig.cmake.in                            ########
+
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+macro(set_and_check _var _file)
+  set(${_var} "${_file}")
+  if(NOT EXISTS "${_file}")
+    message(FATAL_ERROR "File or directory ${_file} referenced by variable ${_var} does not exist !")
+  endif()
+endmacro()
+
+macro(check_required_components _NAME)
+  foreach(comp ${${_NAME}_FIND_COMPONENTS})
+    if(NOT ${_NAME}_${comp}_FOUND)
+      if(${_NAME}_FIND_REQUIRED_${comp})
+        set(${_NAME}_FOUND FALSE)
+      endif()
+    endif()
+  endforeach()
+endmacro()
+
+####################################################################################
+
+include(CMakeFindDependencyMacro)
+
+set(_aja_libs
+    ntv2
+)
+
+foreach(
+  _aja_lib
+  ${_aja_libs}
+)
+  include(${CMAKE_CURRENT_LIST_DIR}/aja_${_aja_lib}_lib.cmake)
+endforeach()
+
+unset(_aja_libs)
